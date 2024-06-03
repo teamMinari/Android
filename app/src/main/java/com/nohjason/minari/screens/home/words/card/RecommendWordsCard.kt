a18ba31f6e577ad34cab3661af88f85c1dc4bd4c
seak23 <113758643+seak230@users.noreply.github.com>
2024-06-03 20:25:46 +0900

    fix: RecommendWordCard
    나오지 않는 텍스트 수정
    모든 용어 더미 데이터 추가

--- app/src/main/java/com/nohjason/minari/screens/home/words/card/RecommendWordsCard.kt
+++ app/src/main/java/com/nohjason/minari/screens/home/words/card/RecommendWordsCard.kt
@@ -6,28 +6,49 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.nohjason.minari.R
 import com.nohjason.minari.screens.ui.text.MinariText
 
 @Composable
-fun RecommedWordsCard() {
-    Row(
+fun RecommedWordsCard(
+    title: String,
+    starCount: Int
+) {
+    Box(
         modifier = Modifier
-            .background(Color.Gray)
-            .width(100.dp)
+            .height(35.dp)
+            .width(150.dp)
+            .clip(RoundedCornerShape(5.dp))
+            .background(Color.LightGray)
+            .padding(5.dp)
     ) {
-        MinariText(text = "펀드")
-    }
-}
+        Column {
+            MinariText(text = title, size = 10)
+            LazyRow {
+                items(starCount) {
+                    Icon(
+                        painter = painterResource(R.drawable.star),
+                        contentDescription = null,
+                        modifier = Modifier.size(7.dp),
+                        tint = Color.Unspecified
+                    )
+                }
+            }
+        }
 
-@Preview(showBackground = true)
-@Composable
-fun Test() {
-    RecommedWordsCard()
+    }
 }
