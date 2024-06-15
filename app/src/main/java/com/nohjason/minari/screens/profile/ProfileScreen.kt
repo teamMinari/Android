ae4d0af769d7fcea508ce44fd17c61144218a5cf
wlals6691 <127966468+wlals6691@users.noreply.github.com>
2024-06-15 23:52:54 +0900

    profile screen update(1)

--- app/src/main/java/com/nohjason/minari/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/nohjason/minari/screens/profile/ProfileScreen.kt
@@ -187,9 +187,9 @@ fun ProfileScreen(
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun ProfileScreenPreview() {
-    val navController = rememberNavController()
-    ProfileScreen(navController = navController)
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun ProfileScreenPreview() {
+//    val navController = rememberNavController()
+//    ProfileScreen(navController = navController)
+//}
